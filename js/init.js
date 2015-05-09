89725a8e1483f97ff05daf120298aa5d5ce6e215
Ben Newman <bn@cs.stanford.edu>
2015-05-09 01:34:12 -0700

    Use highlight.js instead of CodeMirror.

--- js/init.js
+++ js/init.js
@@ -1,19 +1,3 @@
-Array.prototype.forEach.call(
-  document.querySelectorAll("pre[data-language]"),
-  function(pre) {
-    makeCodeMirror(pre);
-  }
-);
-
-var mobilePattern = /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i;
-Reveal.addEventListener("ready", function() {
-  makeCodeMirror.init();
-  if (navigator.userAgent.match(mobilePattern)) {
-    alert("Warning: this presentation is large and contains multitudes, so " +
-          "mobile performance may be disappointing.");
-  }
-});
-
 // Full list of configuration options available at:
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
@@ -39,6 +23,15 @@ Reveal.initialize({
     src: 'plugin/markdown/markdown.js',
     condition: function() {
       return !!document.querySelector( '[data-markdown]' );
+    }
+  }, {
+    src: 'plugin/highlight/highlight.js',
+    async: true,
+    condition: function() {
+      return !!document.querySelector( 'pre' );
+    },
+    callback: function() {
+      hljs.initHighlightingOnLoad();
     }
   }, {
     src: 'plugin/zoom-js/zoom.js',
